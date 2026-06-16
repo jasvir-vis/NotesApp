d5a715d42f2fc80a11d85f02cfd50ed842f0369d
Jasvir singh <159865153+jasvir-vis@users.noreply.github.com>
2026-06-16 16:51:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,13 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:5173",
+      "https://notes-app-lime-five.vercel.app/"
+    ],
+    credentials: true,
+  }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
